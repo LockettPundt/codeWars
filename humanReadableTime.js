3716aeb03250833ee6a29bcc64b93a4d7bf5dc8d
Lockett Pundt <kalifornium@hotmail.com>
2020-01-18 18:36:41 -0500

    takes the time and returns a human readable version of it.

---
+++ humanReadableTime.js
@@ -0,0 +1,25 @@
+function humanReadable(seconds) {
+    let hours = 0;
+    let minutes = 0;
+    if (seconds === 60) {
+      return '00:01:00';
+    }
+    while (seconds >= 60) {
+     seconds -= 60;
+     minutes++;
+    }
+    while (minutes >= 60) {
+      minutes -= 60;
+      hours++;
+    }
+    if (seconds.toString().length === 1) {
+      seconds = '0' + seconds;
+    }
+    if (hours.toString().length === 1) {
+      hours = '0' + hours;
+    }
+    if (minutes.toString().length === 1) {
+      minutes = '0' + minutes;
+    }
+    return (hours + ':' + minutes + ':' + seconds);
+  }
