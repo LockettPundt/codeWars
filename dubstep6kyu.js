2ee7588730e215093b52b70f9e39733e8154add3
Lockett Pundt <kalifornium@hotmail.com>
2020-01-18 18:30:13 -0500

    takes a string and removes the WUBWUB WUB WUB from it and returns the difference in a new string.

---
+++ dubstep6kyu.js
@@ -0,0 +1,16 @@
+function songDecoder(song){
+    let newSong = '';
+    if (song.match(/(WUB)/g)) {
+       newSong = song.replace(/(WUB)+/g, " ");
+    }
+    else {
+      newSong += song;
+    }
+    while (newSong[0] === " ") {
+       newSong = newSong.substr(1);
+    }
+     while (newSong.endsWith(" ")) {
+       newSong = newSong.slice(0, -1)
+    }
+    return newSong;
+   }
