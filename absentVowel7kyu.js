d190cc616bca7247481e5925361033b4e2a869ca
Lockett Pundt <kalifornium@hotmail.com>
2020-01-18 18:28:29 -0500

    takes a string a finds returns the vowel that is missing throughout.

---
+++ absentVowel7kyu.js
@@ -0,0 +1,20 @@
+function absentVowel(x){
+  
+    let strArr = x.match(/[aeiou]/gi);
+    if (!strArr.includes("a")) {
+        return 0;
+    }
+    if (!strArr.includes("e")) {
+        return 1;
+    }
+    if (!strArr.includes("i")) {
+        return 2;
+    }
+    if (!strArr.includes("o")) {
+        return 3;
+    }
+    if (!strArr.includes("u")) {
+        return 4;
+    }
+    
+  }
