13cc98c735a2404c3de2c7e8845b4b3fd841088f
Lockett Pundt <kalifornium@hotmail.com>
2020-01-19 21:18:37 -0500

    take a string and returns a new string with each '#' replaced by a backspace.

---
+++ backspaceInString6kyu.js
@@ -0,0 +1,24 @@
+
+// Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+// Your task is to process a string with "#" symbols.
+
+// Examples
+// "abc#d##c"      ==>  "ac"
+// "abc##d######"  ==>  ""
+// "#######"       ==>  ""
+// ""              ==>  ""
+
+
+function cleanString(s) {
+	let newString = [];
+  let inputArr = s.split("");
+  for (let i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] !== "#") {
+      newString.push(inputArr[i]);
+    } else {
+      newString.pop();
+    }
+  }
+  return newString.join("");
+};
