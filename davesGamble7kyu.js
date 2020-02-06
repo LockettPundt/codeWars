ece2d65224d490de2f1f38e293d0b840d2efdf0d
Lockett Pundt <kalifornium@hotmail.com>
2020-02-05 20:22:35 -0500

    takes a single number and if it's an integer, will returns how many combinations of 3rd place finishes possible amongst the horses.

---
+++ davesGamble7kyu.js
@@ -0,0 +1,11 @@
+//takes a single number and if it's an integer, will returns how many combinations of 3rd place finishes possible amongst the horses.
+
+function horses(n) {
+    if (n !== parseInt(n)) {
+      return undefined;
+    } else if ((n * ( n - 1) * (n - 2)) > 0) {
+      return n * ( n - 1) * (n - 2);
+    } else {
+      return n;
+    }
+  }
