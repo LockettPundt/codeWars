c22c4aae0493c1e0ec2e7052db3229513342612e
Lockett Pundt <kalifornium@hotmail.com>
2020-02-06 19:06:58 -0500

    takes a 2 strings of 18 numbers. one of score and one of the corresponding pars. the function returns the score.

---
+++ whatsMyGolfScore7kyu.js
@@ -0,0 +1,11 @@
+//takes a string of 18 scores and pars and returns the overal score. 
+
+function golfScoreCalculator(parList, scoreList) {
+    let score = 0;
+    let parArr = parList.split('');
+    let scoreArr = scoreList.split('');
+    scoreArr.forEach((strokes, index) => {
+      score += parseInt(strokes) - parseInt(parArr[index]);
+    });
+    return score;
+  }
