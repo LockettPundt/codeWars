bcce095c5eec4f359b6fae65bb09a898e19ab51b
Lockett Pundt <kalifornium@hotmail.com>
2020-02-07 19:22:24 -0500

    refactored to be more concise.

--- pairsOfBears6kyu.js
+++ pairsOfBears6kyu.js
@@ -2,17 +2,7 @@
 //to or larger than the integer argument. 
 
 function bears(x, s){
-    let finalArr = [];
-    let splitString = s.split('');
-    splitString.forEach((item, index) => {
-      if ((item === "B" && splitString[index + 1] === "8") || (item === "8" && splitString[index + 1] === "B")) {
-          finalArr.push(`${item}${splitString[index + 1]}`);
-          splitString.splice(index, 1);
-      }
-    });
-    let bool = false;
-    if (finalArr.length >= x) {
-      bool = true;
-    }
-    return [finalArr.join(''), bool];
-  }
+  let arrMatch = s.match(/(B8)|(8B)/g) || [];
+  let bool = (arrMatch.length >= x);
+  return [arrMatch.join(''), bool];
+}
