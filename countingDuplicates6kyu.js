51b40d67ff95d1bf206eb8f62978f297b9859385
Lockett Pundt <kalifornium@hotmail.com>
2020-02-10 09:15:20 -0500

    Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.

---
+++ countingDuplicates6kyu.js
@@ -0,0 +1,8 @@
+//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur 
+//more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+function duplicateCount(text){
+    let count = [];
+    text.toLowerCase().split('').forEach((a, b) => text.slice(0, b, text.slice(b)).includes(a) && !count.includes(a) ?  count.push(a) : 0);
+    return count.length;
+  }
