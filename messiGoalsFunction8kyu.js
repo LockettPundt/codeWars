ff29d926928dc3d055b7047a5b0ee8d81a8f7d81
Lockett Pundt <kalifornium@hotmail.com>
2020-02-17 16:46:45 -0500

    code a function that adds up all messi's goals in three languages.

---
+++ messiGoalsFunction8kyu.js
@@ -0,0 +1,7 @@
+//Messi is a soccer player with goals in three leagues:
+//Complete the function to return his total number of goals in all three leagues.
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return Array.from(arguments).reduce((a, b) => a + b);
+  }
