4d7b4b78182d53e0239b0d542ea13d6d288adb4b
Lockett Pundt <kalifornium@hotmail.com>
2020-03-30 18:30:49 -0400

    Pirates have notorious difficulty with enunciating. They tend to blur all the letters together and scream at people.
    At long last, we need a way to unscramble what these pirates are saying.
    
    Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.

---
+++ arrh,grabscrab6kyu.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+// Pirates have notorious difficulty with enunciating.
+// They tend to blur all the letters together and scream at people.
+// At long last, we need a way to unscramble what these pirates are saying.
+// Write a function that will accept a jumble of letters as well as a dictionary
+// and output a list of words that the pirate might have meant.
+
+function grabscrab(anagram, dictionary) {
+  return dictionary.filter((element) => anagram.split('').sort().join('') === element.split('').sort().join(''));
+}
