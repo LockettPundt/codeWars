79f8125a17883e401bb021da46a66587ac6aeceb
Lockett Pundt <kalifornium@hotmail.com>
2020-08-02 18:32:33 -0400

    Given a list lst and a number N, create a new list that contains each number of lst at most N times without reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].

---
+++ deleteOccurencesOfElementMoreThanNTimes6kyu.js
@@ -0,0 +1,17 @@
+// Given a list lst and a number N, create a new list that
+// contains each number of lst at most N times without
+// reordering. For example if N = 2, and the input is [1,2,3,1,2,1,2,3],
+// you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1
+// and 2 being in the result 3 times, and then take 3, which
+// leads to [1,2,3,1,2,3].
+
+
+function deleteNth(arr, n) {
+  let items = arr;
+  for (let i = items.length; i > 0; i--) {
+    if (items.filter((num) => num === items[i]).length > n) {
+      items = [...items.slice(0, i), ...items.slice(i + 1)];
+    }
+  }
+  return items;
+}
