15d2bc89a560dfd76c9b32cb0761a3f76bc2ef20
Lockett Pundt <kalifornium@hotmail.com>
2021-03-06 09:00:04 -0500

    feat(codeWars): :art: We will call a natural number a "doubleton number" if it contains exactly two distinct digits. For example, 23, 35, 100, 12121 are doubleton numbers, and 123 and 9980 are not.  For a given natural number n (from 1 to 1 000 000), you need to find the next doubleton number. If n itself is a doubleton, return the next bigger than n.

---
+++ doubletonNumber7kyu.js
@@ -0,0 +1,13 @@
+// We will call a natural number a "doubleton number" if it
+// contains exactly two distinct digits. For example, 23, 35, 100,
+// 12121 are doubleton numbers, and 123 and 9980 are not.
+
+// For a given natural number n (from 1 to 1 000 000), you need
+// to find the next doubleton number. If n itself is a
+// doubleton, return the next bigger than n.
+
+function doubleton(num, start = true) {
+  const numSet = [...new Set(String(num).split(''))].length === 2;
+  if (numSet && !start) return num;
+  return doubleton(num + 1, false);
+}
